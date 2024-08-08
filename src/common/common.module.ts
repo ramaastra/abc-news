7800c6c503d4be4e47fc1850a651e036c025fa18
Rama Astra <ramaastra333@gmail.com>
2024-08-09 00:39:58 +0800

    feat: create ValidationService in CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { ErrorFilter } from './error.filter';
 import { PrismaService } from './prisma.service';
 import { BcryptService } from './bcrypt.service';
+import { ValidationService } from './validation.service';
 
 @Global()
 @Module({
-  providers: [ErrorFilter, PrismaService, BcryptService],
-  exports: [PrismaService, BcryptService],
+  providers: [ErrorFilter, PrismaService, BcryptService, ValidationService],
+  exports: [PrismaService, BcryptService, ValidationService],
 })
 export class CommonModule {}
