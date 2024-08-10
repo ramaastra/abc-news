725c6e654710efc7e45de5af376ba916977647a3
Rama Astra <ramaastra333@gmail.com>
2024-08-11 01:52:10 +0800

    feat: update AuthGuard to evaluate user's role

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,27 +6,55 @@ import {
   HttpStatus,
   Injectable,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { Role } from '@prisma/client';
+import { PrismaService } from '../common/prisma.service';
+import { JwtTokenPayload } from '../models/auth.model';
+import { ROLES_KEY } from './auth.decorator';
+import { UserResponse } from '../models/user.model';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request.headers);
-
     if (!token) {
       throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
     }
 
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_SECRET,
+      const payload: JwtTokenPayload = await this.jwtService.verifyAsync(
+        token,
+        { secret: process.env.JWT_SECRET },
+      );
+      const user: UserResponse = await this.prismaService.user.findUnique({
+        where: { id: payload.sub },
       });
-      request['user'] = payload;
+      request['user'] = user;
     } catch {
       throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles.length) {
+      return true;
+    }
+
+    const isAuthorized = requiredRoles.some(
+      (role) => request.user?.role === role,
+    );
+    if (!isAuthorized) {
+      throw new HttpException('Forbidden resource', HttpStatus.FORBIDDEN);
     }
 
     return true;
