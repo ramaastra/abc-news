f1d4c614b33441e2e94efe76ca2223cab3684694
Rama Astra <ramaastra333@gmail.com>
2024-08-12 13:14:11 +0800

    feat: create @User decorator to retrieve user data in request

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -1,4 +1,9 @@
-import { SetMetadata } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+  SetMetadata,
+} from '@nestjs/common';
 import { Role } from '@prisma/client';
 
 export const ROLES_KEY = 'roles';
@@ -6,3 +11,15 @@ export const Auth = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const User = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    if (user) {
+      return user;
+    } else {
+      throw new HttpException('Unauthorized', 401);
+    }
+  },
+);
